df397d19819929aeaa8c92b2d3d2aa30750a13a4
utku-guclu <utku_guclu@hotmail.com>
2024-12-16 20:18:48 +0300

    repetitions

--- kata/cses/repetitions.cpp
+++ kata/cses/repetitions.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void repetitions(string dna_sequence) {
+int repetitions(string dna_sequence) {
   // find the longest repetition in the sequence
-  int longest_repetition = 0;
-  int current_repetition = 0;
+  int longest_repetition = 1;
+  int current_repetition = 1;
 
   for (int i = 0; i < dna_sequence.length(); i++) {
     if (dna_sequence[i] == dna_sequence[i + 1]) {
@@ -13,16 +13,19 @@ void repetitions(string dna_sequence) {
       if (current_repetition > longest_repetition) {
         longest_repetition = current_repetition;
       }
-      current_repetition = 0;
+      current_repetition = 1;
     }
   }
+
+  return longest_repetition;
 }
+
 int main() {
   // get a dna sequence from user
   string dna_sequence;
   cin >> dna_sequence;
   // get the number of times to repeat
-  repetitions(dna_sequence);
+  cout << repetitions(dna_sequence) << "\n";
 
   return 0;
 }
